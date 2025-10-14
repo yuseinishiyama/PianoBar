7d5916684b589958268f3675a04cbb374c031807
Yusei Nishiyama <yusei.nishiyama@shopify.com>
2025-10-13 21:29:32 -0600

    Initial menu bar app setup
    - Create PianoBar as a menu bar only application
    - Add status item with music note icon
    - Implement basic menu with disconnect status and quit option
    - Configure app to run without dock icon (LSUIElement)

--- PianoBar/PianoBarApp.swift
+++ PianoBar/PianoBarApp.swift
@@ -6,12 +6,34 @@
 //
 
 import SwiftUI
+import AppKit
 
 @main
 struct PianoBarApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem!
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "music.note", accessibilityDescription: "PianoBar")
+        }
+
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "MIDI: Disconnected", action: nil, keyEquivalent: ""))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit PianoBar", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+
+        statusItem.menu = menu
     }
 }
