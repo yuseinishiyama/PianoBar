e211ee235a0b1dccc3bcdb582d33e3c63c516a29
Yusei Nishiyama <yusei.nishiyama@shopify.com>
2025-10-13 19:20:30 -0600

    Initial Commit

---
+++ PianoBar/PianoBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PianoBarApp.swift
+//  PianoBar
+//
+//  Created by Yusei Nishiyama on 2025-10-13.
+//
+
+import SwiftUI
+
+@main
+struct PianoBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
